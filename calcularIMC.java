a7380a12614e87fa59c47f5511b64da59f976760
Ángel Montero Morán <angel.montero.moran@gmail.com>
2024-11-17 10:42:44 +0100

    Update calcularIMC.java

--- calcularIMC.java
+++ calcularIMC.java
@@ -5,44 +5,43 @@ import java.util.Scanner;
 public class calcularIMC {
 
 	public static void main(String[] args) {
-		float peso, altura, imc;  //Declaramos las variables que vamos a usar
+		float peso, altura, imc;  
 		boolean bandera1, bandera2;
 		
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("¡Los decimales deben separarse con coma (,) no con punto (.)!"); // Aclaración para avitar errores
+		System.out.println("¡Los decimales deben separarse con coma (,) no con punto (.)!"); 
 		
-		System.out.println("Introduce el peso el kg, min 30,0kg max 300,0kg:"); //Solicitamos al usuario el peso
+		System.out.println("Introduce el peso el kg, min 30,0kg max 300,0kg:"); 
 		
-		peso = sc.nextFloat(); //Almacenamos el peso
+		peso = sc.nextFloat(); 
 		
-		System.out.println("Introduce la altura en m, min 1,30m max 2,00m:"); //Solicitamos al usuario la altura
+		System.out.println("Introduce la altura en m, min 1,30m max 2,00m:"); 
 		
-		altura = sc.nextFloat(); //Almacenamos la altura
+		altura = sc.nextFloat(); 
 		
-		bandera1 = true;  //Creamos dos banderas para usarlas en dos bucles while
-		bandera2 = true;  //Inicializamos en true
+		bandera1 = true;  
+		bandera2 = true;  
 		
-		if (peso <= 29 || peso >= 301){  //Comprobamos que el peso está en el rango
-			bandera1 = false; //Si no está en el rango, cambiamos la bandera a false
-		}
+		if (peso <= 29 || peso >= 301){  
+			bandera1 = false; 
 		
-		if (altura <= 1.29 || altura >= 2.0){ //Repetimos para la altura
+		if (altura <= 1.29 || altura >= 2.0){ 
 			bandera2 = false; 
 		}
 		
 		
-		while (bandera1 == false) { //Bucle si el peso no está en el rango
+		while (bandera1 == false) { 
 			
 			System.out.println ("¡El peso es incorrecto!");
 			System.out.println("Vuelve a introducir el peso, min 30kg max 300kg:");
 			peso = sc.nextFloat();
-				if (peso >= 29 && peso <= 301){ //Comprobamos de nuevo el rago
-					bandera1 = true; //Cambiamos valor de bandera para salir del bucle
+				if (peso >= 29 && peso <= 301){ 
+					bandera1 = true; 
 				}
 		}
 		
-		while (bandera2 == false) { //Repetimos para la altura
+		while (bandera2 == false) { 
 			
 			System.out.println ("¡La altura es incorrecta!");
 			System.out.println("Vuelve a introducir la altura, min 1,30m max 2m:");
@@ -52,10 +51,10 @@ public class calcularIMC {
 				}
 		}
 		
-		imc = peso / (altura * altura); //Calculamos IMC
+		imc = peso / (altura * altura); 
 		String resultado; 
 		
-		if (imc < 18.5) {  //Comprobamos e imprimimos el resultado según los rangos de la tabla
+		if (imc < 18.5) {  
 			resultado = "Bajo peso";
 			System.out.println("Tu IMC = " + imc);
 			System.out.println("El resultado es: " + resultado);		
@@ -77,4 +76,5 @@ public class calcularIMC {
 		} sc.close();
 
 	}
+}
 }
